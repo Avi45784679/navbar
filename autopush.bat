1ac88af57cdf541161d3c7ebe4cf895624c83bbf
Avinash Lakhane <Avinash@mobitrail.local>
2025-11-01 20:52:41 +0530

    Auto update on 01-11-2025 20:52:40.83

--- autopush.bat
+++ autopush.bat
@@ -1,5 +1,17 @@
 @echo off
-echo Auto pushing to GitHub...
+cd /d "D:\New folder\a\Full Stack Web Developement Mastery Course\navbarcss"
+echo.
+echo ============================
+echo  Auto Push to GitHub Started
+echo ============================
+echo.
+
 git add .
-git commit -m "Auto update %date% %time%"
+git commit -m "Auto update on %date% %time%"
 git push origin main
+
+echo.
+echo ============================
+echo  Push Complete!
+echo ============================
+pause
